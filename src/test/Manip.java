a5a12c41ed865dd08fd84d2be734da9ebd7fb5b3
Rico97129 <richardson.thicot@outlook.fr>
2023-02-08 14:54:14 +0100

    ajoute getdev1() dans manip

--- src/test/Manip.java
+++ src/test/Manip.java
@@ -1,7 +1,14 @@
 package test;
 
 public class Manip {
+	public int getMain() {
+		return main1;
+	}
 private int main1;
 private int main2;
 private int dev1;
+public int getDev1() {
+	return dev1;
+}
+
 }
