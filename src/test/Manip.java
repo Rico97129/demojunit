7414d0618dcc0101c451ed805c967395147b4bd9
Rico97129 <richardson.thicot@outlook.fr>
2023-02-08 14:57:38 +0100

    hello

--- src/test/Manip.java
+++ src/test/Manip.java
@@ -1,5 +1,14 @@
 package test;
 
 public class Manip {
+	public int getMain() {
+		return main1;
+	}
 private int main1;
+private int main2;
+private int dev1;
+public int getMain2() {
+	return dev1;
+}
+
 }
